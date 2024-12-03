edf5161b99f95eb8a7ade7810c01f947bdacde83
lostmart <lostmart@hotmail.com>
2024-12-03 10:39:14 +0100

    new stuff

--- script.js
+++ script.js
@@ -3,16 +3,3 @@ import { button } from "./components/compBtn.js"
 import { eclipseComp } from "./components/eclipseComp.js"
 import { headerComp } from "./components/headerComp.js"
 import { showModal } from "./services/services.js"
-
-app.appendChild(headerComp())
-app.appendChild(
-	button("First Button", () => showModal("secondary-btn"), "secondary-btn")
-)
-app.appendChild(
-	button("Second button", () => showModal("primary-btn"), "primary-btn")
-)
-app.appendChild(
-	button("Third button", () => showModal("third-color"), "third-color")
-)
-
-app.appendChild(eclipseComp("primary"))
